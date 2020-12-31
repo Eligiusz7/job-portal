6a0da8ccedfce4eca1ccebe532c32d33b6f56703
Eligiusz Kurzawa <eligiusz.kurzawa@example.com>
2020-12-31 11:11:30 +0100

    5# added category model

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,18 @@ from django.template.defaultfilters import slugify
 from jobportal import settings
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(default=None, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+
 class Job(models.Model):
     title = models.CharField(max_length=300)
     company = models.CharField(max_length=300)
@@ -21,6 +33,7 @@ class Job(models.Model):
     publishing_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(default=None, editable=False)
     employer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="jobs", default=None)
 
     def __str__(self):
         return self.title
