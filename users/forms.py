f8c4829d5529fbcd9f3b5c84f4f7909c8fc905e7
Eligiusz Kurzawa <eligiusz.kurzawa@example.com>
2021-01-11 20:40:44 +0100

    10# created user profile update form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from users.models import Account
+from users.models import Account, Profile
 
 
 class AccountRegisterForm(UserCreationForm):
@@ -10,3 +10,13 @@ class AccountRegisterForm(UserCreationForm):
     class Meta:
         model = Account
         fields = ['email', 'first_name', 'last_name']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
