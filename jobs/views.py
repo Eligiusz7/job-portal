21c6e3d723e0be43984e80161945d600d2ffec0d
Eligiusz Kurzawa <eligiusz.kurzawa@example.com>
2021-01-13 21:11:38 +0100

    15# created category detail page

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 
 from django.views.generic import TemplateView, ListView, CreateView, DetailView
@@ -12,7 +13,7 @@ class HomeView(ListView):
     template_name = 'jobs/index.html'
     context_object_name = 'jobs'
     model = Job
-    paginate_by = 1
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
@@ -44,3 +45,22 @@ class SingleJobView(DetailView):
         context = super(SingleJobView, self).get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
+
+
+class CategoryDetailView(ListView):
+    model = Job
+    template_name = 'jobs/category-detail.html'
+    context_object_name = 'jobs'
+    paginate_by = 2
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return Job.objects.filter(category=self.category)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(CategoryDetailView, self).get_context_data(*args, **kwargs)
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        context['categories'] = Category.objects.all()
+        context['category'] = self.category
+        return context
+
