ab903cb96609c7ec116ecde62ef41c4ab029ed01
Eligiusz Kurzawa <eligiusz.kurzawa@example.com>
2021-01-15 16:03:10 +0100

    24# added messages2

--- users/views.py
+++ users/views.py
@@ -93,3 +93,14 @@ class EmployerPostedJobsView(ListView):
 
     def get_queryset(self):
         return Job.objects.filter(employer=self.request.user).order_by('-id')
+
+
+@method_decorator(login_required(login_url='/users/login'), name='dispatch')
+class EmployeeMessagesView(ListView):
+    model = Job
+    template_name = 'users/employee-messages.html'
+    paginate_by = 5
+    context_object_name = 'jobs'
+
+    def get_queryset(self):
+        return Job.objects.filter(invites__isnull=False, invites__user_id=self.request.user).order_by('-invites')
