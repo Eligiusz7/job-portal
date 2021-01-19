4a1a8ec37d03afdf012e454b4a5d6296ccae10f8
Eligiusz Kurzawa <eligiusz.kurzawa@example.com>
2021-01-19 10:01:45 +0100

    43# changed database to sqllite

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -83,17 +83,11 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobportal',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            "init_command": "SET foreign_key_checks = 0",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
